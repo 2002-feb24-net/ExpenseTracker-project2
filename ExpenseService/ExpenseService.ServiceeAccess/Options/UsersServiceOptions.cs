666c9b0607d260dddcf5d8882036f541ae125337
Tyler <claypooltyler00@gmail.com>
2020-04-17 18:21:21 -0500

    Added add and getAll methods

---
+++ ExpenseService/ExpenseService.ServiceeAccess/Options/UsersServiceOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseService.ServiceeAccess.Options
+{
+    class UsersServiceOptions
+    {
+        public string ServiceBaseUrl { get; set; }
+    }
+}

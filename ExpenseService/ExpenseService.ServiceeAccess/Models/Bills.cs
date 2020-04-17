ff219356e4a61c694b4d7b99d1660af1f71977f6
Tyler <claypooltyler00@gmail.com>
2020-04-17 17:13:43 -0500

    Movded data model entites in seperate project

---
+++ ExpenseService/ExpenseService.ServiceeAccess/Models/Bills.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpenseServiceAPI.Models
+{
+    public partial class Bills
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string PurchaseName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime BillDate { get; set; }
+        public string Location { get; set; }
+
+        public virtual Users User { get; set; }
+    }
+}

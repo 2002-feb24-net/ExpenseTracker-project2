af13bde5c6c8ad40df1d19299c0885d5349338a2
Tyler <claypooltyler00@gmail.com>
2020-04-17 17:42:31 -0500

    Added interface for user repository

---
+++ ExpenseService/ExpenseService.ServiceeAccess/Interrfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using ExpenseServiceAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseService.ServiceeAccess.Interrfaces
+{
+    //Interface for the Users repository
+    interface IUserService
+    {
+        //Adds a user while using async
+        Task AddUserAsync(Users user);
+
+        //Gets all users while using async
+        Task<IEnumerable<Users>> GetUsersASync();
+
+    }
+}

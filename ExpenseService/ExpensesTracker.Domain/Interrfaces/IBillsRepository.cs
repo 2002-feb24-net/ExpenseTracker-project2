d494a3b966b1e56b36a324120ac31631e37625fe
Tyler <claypooltyler00@gmail.com>
2020-04-19 13:14:35 -0500

    Implemented the IBillRepository

---
+++ ExpenseService/ExpensesTracker.Domain/Interrfaces/IBillsRepository.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseService.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseService.Domain.Interrfaces
+{
+    interface IBillsRepository
+    {
+        Task<IEnumerable<Bills>> GetBillsAsync();
+        Task<Bills> GetBillByIdAsync(int id);
+        Task<bool> BillExsistsAsync(int id);
+        Task<Users> AddBillAsync(Users users);
+        Task<bool> RemoveBillAsync(int id);
+        public EntityState Changed(Bills bills);
+
+        Task SaveAsync();
+    }
+}

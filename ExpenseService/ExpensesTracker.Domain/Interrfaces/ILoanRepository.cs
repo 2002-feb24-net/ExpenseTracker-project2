8298b90ae7a74df617ff4fe7274b89e38e84a7ca
Tyler <claypooltyler00@gmail.com>
2020-04-19 17:50:57 -0500

    Loan repository half finsihed

--- ExpenseService/ExpensesTracker.Domain/Interrfaces/ILoanRepository.cs
+++ ExpenseService/ExpensesTracker.Domain/Interrfaces/ILoanRepository.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace ExpenseService.Domain.Interrfaces
 {
-    interface ILoanRepository
+    public interface ILoanRepository
     {
         Task<IEnumerable<Loan>> GetLoanAsync(int? userId = null);
         Task<Loan> GetLoanByIdAsync(int id);
         Task<bool> LoanExsistsAsync(int id);
         Task<Loan> AddLoanAsync(Loan Loan);
         Task<bool> RemoveLoanAsync(int id);
-        public EntityState Changed(Loan Loan);
+        EntityState Changed(Loan Loan);
 
         Task SaveAsync();
     }

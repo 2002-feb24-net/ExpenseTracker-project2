502370b89ec056533992080f0d5e458b6439a03b
Tyler <claypooltyler00@gmail.com>
2020-04-19 19:36:25 -0500

    Added application to Domain models

---
+++ ExpenseService/ExpensesTracker.Domain/Interrfaces/Application.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseService.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseService.Domain.Interrfaces
+{
+    class Application
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Ssn { get; set; }
+        public decimal CreditScore { get; set; }
+        public decimal EstIncome { get; set; }
+        public bool? ApprovalDenialComformation { get; set; }
+        public decimal LoanAmount { get; set; }
+
+        public Users User { get; set; }
+    }
+}

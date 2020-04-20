8d503d6814c85c902bcf304966c3666a9d45af7b
Tyler <claypooltyler00@gmail.com>
2020-04-19 20:30:46 -0500

    Add subscriptions model to domain

---
+++ ExpenseService/ExpensesTracker.Domain/Model/Subscriptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseService.Domain.Model
+{
+    class Subscriptions
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Company { get; set; }
+        public string SubscriptionName { get; set; }
+        public decimal SubscriptionMonthCost { get; set; }
+        public DateTime SubscriptionDate { get; set; }
+        public DateTime SubscriptionDueDate { get; set; }
+        public bool Notification { get; set; }
+
+        public Users User { get; set; }
+    }
+}

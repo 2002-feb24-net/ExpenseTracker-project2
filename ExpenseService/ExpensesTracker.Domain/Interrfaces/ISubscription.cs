3765790d07e72f833816eec3eacdd21536159ac2
Tyler <claypooltyler00@gmail.com>
2020-04-19 20:34:49 -0500

    added Isubscription

---
+++ ExpenseService/ExpensesTracker.Domain/Interrfaces/ISubscription.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseService.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseService.Domain.Interrfaces
+{
+    interface ISubscription
+    {
+        Task<IEnumerable<Subscriptions>> GetSubscriptionssAsync(int? userId = null);
+        Task<Subscriptions> GetSubscriptionsByIdAsync(int id);
+        Task<bool> SubscriptionsExsistsAsync(int id);
+        Task<Subscriptions> AddSubscriptionsAsync(Subscriptions Subscriptions);
+        Task<bool> RemoveSubscriptionsAsync(int id);
+        public EntityState Changed(Subscriptions Subscriptions);
+
+        Task SaveAsync();
+    }
+}

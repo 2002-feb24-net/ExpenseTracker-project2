2efe41dddeb1f02ac2913738c93c86e6de7453fa
Jacob Koch <jacob.koch.utd@gmail.com>
2020-04-22 14:57:36 -0500

    added swagger

--- ExpenseService/Startup.cs
+++ ExpenseService/Startup.cs
@@ -9,12 +9,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
+[assembly: ApiController]
 namespace ExpenseService
 {
     public class Startup
@@ -30,28 +33,34 @@ namespace ExpenseService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
+            services.AddApplicationInsightsTelemetry();
             services.AddScoped<IExpensesRepository, ExpensesRepository>();
             services.AddScoped<IBillsRepository, Billrepository>();
             services.AddScoped<IBudgetRepository, BudgetRepository>();
             services.AddScoped<ILoanRepository, LoanRepository>();
             services.AddScoped<IApplication, ApplicationRepository>();
             services.AddScoped<ISubscription, Subscription>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Notes API", Version = "v1" });
+            });
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowLocalAndAppServiceAngular", builder =>
-                    builder.WithOrigins("https://revatureexpensetracker-client.azurewebsites.net/", "http://revatureexpensetracker-client.azurewebsites.net/",
-                                        "http://localhost:4200", "https://localhost:4200")
+                    builder.WithOrigins("https://revatureexpensetracker-client.azurewebsites.net/", 
+                                        "http://localhost:4200")
                         .AllowAnyMethod()
                         .AllowAnyHeader());
             });
             services.AddControllers(options =>
             {
+                options.OutputFormatters.RemoveType<StringOutputFormatter>();
                 options.ReturnHttpNotAcceptable = true;
                 options.SuppressAsyncSuffixInActionNames = false;
             });
             services.AddDbContext<RevatureDatabaseContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("Project2String")));
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,14 +71,20 @@ namespace ExpenseService
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors("AllowLocalAndAppServiceAngular");
-
             app.UseAuthorization();
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("AllowLocalAndAppServiceAngular");
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notes API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
